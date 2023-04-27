4715f0ed330be42c808675f9ec25e9d2acfe23c8
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2023-04-27 01:11:19 -0400

    Create ehpalindromo.java

---
+++ testarPalins/ehpalindromo.java
@@ -0,0 +1,20 @@
+package testarPalins;
+
+public class ehpalindromo {
+
+    protected int numero;
+    protected boolean ehPalindromo;
+    protected int[] palindromos;
+
+    public ehpalindromo(int numero, int[] palindromos) {
+        this.numero = numero;
+        this.palindromos = palindromos;
+        this.ehPalindromo = EhPalindromo();
+    }
+
+    public boolean EhPalindromo() {
+        
+    }
+    
+    
+}
