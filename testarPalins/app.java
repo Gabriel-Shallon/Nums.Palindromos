7f1b280fe06b55ef25387e5243469564f9a4a46d
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2023-04-27 04:32:18 -0400

    Update app.java

--- testarPalins/app.java
+++ testarPalins/app.java
@@ -1,4 +1,17 @@
 package testarPalins;
 
+import javax.swing.JOptionPane;
 
-//não sei oque aconteceu, mas meu JDK parou de funcionar e eu não consigo reinstalar :(
+public class app{
+
+    public static void main(String[] args) {
+        
+
+        palindromos callpalins = new palindromos();
+        int[] palindromoA = callpalins.PrimeirosPalindromos();
+        for (int i = 0; i < palindromoA.length; i++){
+            JOptionPane.showMessageDialog(null, palindromoA[i]+" é o "+(i+1)+"° número palindromo a partir de "+(palindromoA.length-palindromoA[0]));
+        }
+    }
+}
+
