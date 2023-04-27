3a891df9fdea3b26ff52efef51cbcd445509b3e3
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2023-04-27 17:11:48 -0400

    Update app.java

--- testarPalins/app.java
+++ testarPalins/app.java
@@ -2,16 +2,17 @@ package testarPalins;
 
 import javax.swing.JOptionPane;
 
-public class app{
+public class app {
+
+    
 
     public static void main(String[] args) {
-        
-
-        palindromos callpalins = new palindromos();
-        int[] palindromoA = callpalins.PrimeirosPalindromos();
+        int palindromoslength = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantos números serão testados?"));
+        palindromos palindromo = new palindromos(palindromoslength);
+        int[] palindromoA = palindromo.getPalindromos();
         for (int i = 0; i < palindromoA.length; i++){
-            JOptionPane.showMessageDialog(null, palindromoA[i]+" é o "+(i+1)+"° número palindromo a partir de "+(palindromoA.length-palindromoA[0]));
+            JOptionPane.showMessageDialog(null, palindromoA[i] + " é o " + (i+1) + "° número palíndromo a partir do número setado anteriormente.");
         }
+
     }
 }
-
