685e9ceeea87df96b1e4ee1bef675a430560ac8c
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2023-04-27 17:11:44 -0400

    Update ehpalindromo.java

--- testarPalins/ehpalindromo.java
+++ testarPalins/ehpalindromo.java
@@ -1,49 +1,17 @@
 package testarPalins;
 
-public class ehpalindromo extends palindromos{
+public class ehpalindromo {
 
-    
-    protected boolean ehPalindromo;
-    
-    
-    public ehpalindromo(int numero) {
-        super();
-        this.ehPalindromo = EhPalindromo(numero);
+    public static boolean ehPalindromo(int numero) {
+        int numeroInvertido = 0;
+        int resto;
+        int aux = numero;
+        while (aux > 0) {
+            resto = aux % 10;
+            numeroInvertido = numeroInvertido * 10 + resto;
+            aux = aux / 10;
+        }
+        return numero == numeroInvertido;
     }
-
-    
-    public boolean EhPalindromo(int numero) {
-        int numtemp = numero;
-        int numlength = 0;
-        while(numtemp!=0){
-            numtemp/=10;
-            numlength++;
-        }
-        int[] numext = new int[numlength];
-        int resto = numero;
-        for (int i = 0; i < numlength; i++){
-            numext[i] = resto % 10;
-            resto /= 10;
-        }
-
-        int o = 1;
-        for (int i = 0; i < numlength/2; i++){
-            if (numext[i]!=numext[numlength-(i+1)]){
-                o=0;
-            } 
-        }
-
-        if (0==1){
-        return true;
-        }else{
-        return false;  
-        }
-    }
-
-    
-
-
-
-   
-    
+      
 }
